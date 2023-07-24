2620c6324a1f17ef8443132168382960e4d5884e
ES0227 <erinsuh0227@gmail.com>
2023-07-24 13:58:52 -0400

    .

--- dance_template.py
+++ dance_template.py
@@ -1,6 +1,42 @@
 from motor_test import test_motor
 import time
 from pymavlink import mavutil
+import socket
+import sys
+
+
+def arm_rov(mav_connection):
+    """
+    Arm the ROV, wait for confirmation
+    """
+    mav_connection.arducopter_arm()
+    print("Waiting for the vehicle to arm")
+    mav_connection.motors_armed_wait()
+    print("Armed!")
+
+def disarm_rov(mav_connection):
+    """
+    Disarm the ROV, wait for confirmation
+    """
+    mav_connection.arducopter_disarm()
+    print("Waiting for the vehicle to disarm")
+    mav_connection.motors_disarmed_wait()
+    print("Disarmed!")
+
+def run_motors_timed(mav_connection, seconds: int, motor_settings: list) -> None:
+    """
+    Run the motors for a set time
+    :param mav_connection: The mavlink connection
+    :param time: The time to run the motors
+    :param motor_settings: The motor settings, a list of 6 values -100 to 100
+    :return: None
+    """
+    step = 0
+    while step < seconds:
+        for i in range(len(motor_settings)):
+            test_motor(mav_connection=mav_connection, motor_id=i, power=motor_settings[i])
+        time.sleep(0.2)
+        step += 0.2
 
 def letter_A():
     # run_motors_timed(mav_connection, seconds = 2.5, motor_settings=[100, -100, -100, 100, 0, 0])
@@ -33,7 +69,7 @@ def forward(seconds):
 
 def amongus():
     forward(6) #forward
-    run_motors_timed(mav_connection, seconds=10, motor_settings=[- ,-100 ,85 ,100, 0, 0]) #right
+    run_motors_timed(mav_connection, seconds=10, motor_settings=[-85,-100 ,85 ,100, 0, 0]) #right
     forward(6)
     run_motors_timed(mav_connection, seconds=2.5, motor_settings=[0, 100 , 0 ,-100, 0, 0]) #rotate left without moving
     run_motors_timed(mav_connection, seconds=25, motor_settings=[-98,-100 ,98 ,100, 0, 0])
@@ -56,6 +92,8 @@ def amongus():
     forward(5)
 
 
+
+
 if __name__ == "__main__":
     ####
     # Initialize ROV
@@ -72,32 +110,21 @@ if __name__ == "__main__":
     Call sequence of calls to run_timed_motors to execute choreography
     Motors power ranges from -100 to 100
     """
-    
-
-    letter_A()
-
-    run_motors_timed(mav_connection, seconds=2, motor_settings=[0, 0, 0, 0, 0, 0]) # pause
-    run_motors_timed(mav_connection, seconds=1.4, motor_settings=[-100, 100, 100, -100, 0, 0]) # turn
-    run_motors_timed(mav_connection, seconds=4.5, motor_settings=[-100, -100, 50, 50, 0, 0]) # forward
-    run_motors_timed(mav_connection, seconds=1.2, motor_settings=[-100, 100, 100, -100, 0, 0]) # turn
-    run_motors_timed(mav_connection, seconds=4.4, motor_settings=[-100, -100, 50, 50, 0, 0]) # forward
-    run_motors_timed(mav_connection, seconds=2, motor_settings=[0, 0, 0, 0, 0, 0]) # pause
-    run_motors_timed(mav_connection, seconds=3.5, motor_settings=[-100, 100, 100, -100, 0, 0]) # turn
-    letter_U()
-    run_motors_timed(mav_connection, seconds=2.5, motor_settings=[100, -100, -100, 100, 0, 0]) # turn
-
-    letter_V()
-
-    #crab right rotate left
-    run_motors_timed(mav_connection, seconds=23, motor_settings=[100, -100, 100, -100, 0, 0])
-    run_motors_timed(mav_connection, seconds=2.5, motor_settings=[0, 100 , 0 ,-100, 0, 0])
-
-    amongus()
-    
+    HOST =''
+    PORT = 50007
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        print("made socket")
+        s.bind((HOST, PORT))
+        print("compelted")
+        stop = False
+        while not stop:
+            run_motors_timed(mav_connection, seconds=9.5, motor_settings=[-85,-100 ,85 ,100, 0, 0])
+            print("motors are running")
 
     
-    # stop
-    run_motors_timed(mav_connection, seconds=5, motor_settings=[0, 0, 0, 0, 0, 0])
+
+
+    
 
     ####
     # Disarm ROV and exit
---
+++ emily_server.py
@@ -0,0 +1,85 @@
+import socket as skt
+import dance_template
+from pymavlink import mavutil
+
+# creates and returns a socket
+def createSocket():
+    protocol = skt.SOCK_STREAM  # Protocol = TCP
+    addressFamily = skt.AF_INET  # Address family = IPv4
+
+    # socket creation
+    socket = skt.socket(addressFamily, protocol)
+    socket.setsockopt(skt.SOL_SOCKET, skt.SO_KEEPALIVE, 1)
+    # Bind information
+    serverIp = "10.29.117.20"
+    serverPrt = 6500
+
+    # Binding port and IP
+    socket.bind((serverIp, serverPrt))
+
+    return socket
+
+# logs request
+def requestProcessor(request, command_queue):
+    command, time_str = request.decode().split()
+    time = int(time_str)
+
+    commandDict = {
+        "forward": [-100, -100, 100, 100],
+        "backward": [100, 100, -100, -100],
+        "clockwise": [100, -100, -100, 100],
+        "counter_clockwise": [-100, 100, 100, -100],  # Changed the duplicate key
+        "left": [-100, 100, -100, 100],
+        "right": [100, -100, 100, -100],
+        "stop": [0, 0, 0, 0]
+    }
+    if command in commandDict:
+        vector = commandDict[command]
+    else:
+        print("That's not a command.")
+        return
+
+    # Put the command into the queue
+    command_queue.append((vector, time))
+
+# create socket
+socket = createSocket()
+
+# listen for incoming connections
+socket.listen()
+
+try:
+    while True:
+        # accept a connection from the client
+        conn, addr = socket.accept()
+
+        # Create a queue for commands
+        command_queue = []
+
+        while True:
+            # receive data
+            request = conn.recv(1024)
+
+            if not request:
+                # If the client closed the connection, break the loop
+                break
+
+            # Add the command to the queue
+            requestProcessor(request, command_queue)
+
+        # Process the commands in the queue
+        mav_connection = mavutil.mavlink_connection('udpin:0.0.0.0:14550')
+        mav_connection.wait_heartbeat()
+        dance_template.arm_rov(mav_connection)
+
+        for vector, time in command_queue:
+            dance_template.run_motors_timed(mav_connection, seconds=time, motor_settings=vector)
+
+        # Close the connection after processing the requests
+        conn.close()
+
+except KeyboardInterrupt:
+    print("Server stopped.")
+
+# close the server socket
+socket.close()
